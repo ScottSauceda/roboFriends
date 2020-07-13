3ed5cf8bf5e84eb3b9fa1f49e69299d46215b403
Marcus Sauceda <marcus@Marcuss-MBP.lan>
2020-07-13 12:25:47 -0500

    Updates

---
+++ precache-manifest.a84433beb9cc219069b0073cabbc57d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d376b4048e41712524b8b0a584d8fe",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "5629e29903776bee648f",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "50d5d351060f8ac09154",
+    "url": "/roboFriends/static/css/main.d7afef13.chunk.css"
+  },
+  {
+    "revision": "5629e29903776bee648f",
+    "url": "/roboFriends/static/js/2.a9c43ba2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/static/js/2.a9c43ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d5d351060f8ac09154",
+    "url": "/roboFriends/static/js/main.722494e6.chunk.js"
+  },
+  {
+    "revision": "821fe69068fb5b686875",
+    "url": "/roboFriends/static/js/runtime-main.d8e71509.js"
+  }
+]);
