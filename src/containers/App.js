5c29262d1b0da9b51fbb2f46bcc76bb727a4bc32
Marcus Sauceda <marcussauceda@Marcuss-iMac.lan>
2020-07-18 14:46:23 -0500

    added connect function to project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import ErrorBoundary from '../components/ErrorBoundary';
 import Scroll from '../components/Scroll';
@@ -6,30 +7,39 @@ import SearchBox from '../components/SearchBox';
 import './App.css';
 
 
+import { setSearchField } from '../actions'
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: [],
-            searchField: ''
+            robots: []
         }
     }
 
     componentDidMount() {
         // console.log(this.props.store.getState())
-        console.log("apple")
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => this.setState({robots: users}));
         // this.setState({robots: robots});
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-    }
-
     render() {
-        const { robots, searchField } = this.state;
+        const { robots } = this.state;
+            const { searchField, onSearchChange } = this.props;
             const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
@@ -41,7 +51,7 @@ class App extends Component {
             return (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={filteredRobots} />
@@ -53,4 +63,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,15 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import { CHANGE_SEARCH_FIELD } from './constants.js';
 
 const initialState = {
     searchField: ''
 }
 
 export const searchRobots = (state = initialState, action = {}) => {
+    console.log(action.type)
     switch(action.type) {
         case CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, {searchField:action.payload} )
-            // return {...state, searchField:action.payload } // destructured way using spread operator
-        default :
+            return Object.assign({}, state, { searchField: action.payload})
+        default:
             return state;
     }
 }
