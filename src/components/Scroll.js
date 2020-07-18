c8b648a1663287d156b3debb72935210d040670c
Marcus Sauceda <marcussauceda@Marcuss-iMac.lan>
2020-07-18 16:02:28 -0500

    added Redux Logger

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Scroll = (props) => {
-    console.log(props);
+    // console.log(props);
     // return props.children;
     // return <h1>Hi</h1>
     return (
--- src/containers/App.js
+++ src/containers/App.js
@@ -31,7 +31,6 @@ class App extends Component {
 
     componentDidMount() {
         // console.log(this.props.store.getState())
-        console.log("apple");
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => this.setState({robots: users}));
